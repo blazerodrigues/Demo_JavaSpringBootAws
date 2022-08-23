227d953de8fb0e2bac04fffa138668c6e495c72f
Blaze Rodrigues <blazerodrigues@gmail.com>
2022-08-23 23:18:52 +0530

    Initial Commit

---
+++ src/test/java/com/blaze/springbootawsecsfargate/SpringbootawsecsfargateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blaze.springbootawsecsfargate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootawsecsfargateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
